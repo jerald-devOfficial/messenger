4836edd6e08f83c4779c8a0b4a67ac83e59e9583
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2023-10-19 23:12:08 +0800

    Initial commit but finished messenger project

---
+++ src/app/users/layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import Sidebar from '../components/sidebar/Sidebar';
+import getUsers from '../actions/getUsers';
+import UserList from './components/UserList';
+
+export default async function UsersLayout({
+  children
+}: {
+  children: ReactNode;
+}) {
+  const users = await getUsers();
+  return (
+    <Sidebar>
+      <div className='h-full'>
+        <UserList items={users} />
+        {children}
+      </div>
+    </Sidebar>
+  );
+}
